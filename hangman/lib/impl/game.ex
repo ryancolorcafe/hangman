8c12686cf137765cacaaef8135e6d34e5738fe4e
ryancolorcafe <ryan.brown@fastradius.com>
2022-08-18 16:49:07 -0700

    add tests, allow new_game to accept word arg

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,23 +1,26 @@
 defmodule Hangman.Impl.Game do
-
-  @type t :: %Hangman.Impl.Game{
-    turns_left: integer,
-    game_state: Hangman.state,
-    letters:    list(String.t),
-    used:       MapSet.t(String.t),
-  }
+  @type t :: %__MODULE__{
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
 
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters:    [],
-    used:       MapSet.new(),
+    letters: [],
+    used: MapSet.new()
   )
 
   @spec new_game :: t
   def new_game do
-    %Hangman.Impl.Game{
-      letters: Dictionary.random_word |> String.codepoints 
+    new_game(Dictionary.random_word())
+  end
+
+  def new_game(word) do
+    %__MODULE__{
+      letters: word |> String.codepoints()
     }
   end
 end
