258a22621faa9b7e02cd686d68f9b19d4407f472
ryancolorcafe <ryan.brown@fastradius.com>
2022-08-19 13:41:39 -0700

    finish logic for winning, losing take two

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -30,15 +30,58 @@ defmodule Hangman.Impl.Game do
   @spec make_move(t, String.t()) :: {t, Type.tally()}
   def make_move(game = %{game_state: state}, _guess)
       when state in [:won, :lost] do
-    {game, tally(game)}
+    game
+    |> return_with_tally()
+  end
+
+  def make_move(game, guess) do
+    accept_guess(game, guess, MapSet.member?(game.used, guess))
+    |> return_with_tally()
+  end
+
+  defp accept_guess(game, _guess, _already_used = true) do
+    %{game | game_state: :already_used}
+  end
+
+  defp accept_guess(game, guess, _already_used) do
+    %{game | used: MapSet.put(game.used, guess)}
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  defp score_guess(game, _good_guess = true) do
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{game | game_state: new_state}
+  end
+
+  defp score_guess(game = %{turns_left: 1}, _bad_guess) do
+    %{game | game_state: :lost, turns_left: 0}
+  end
+
+  defp score_guess(game, _bad_guess) do
+    %{game | game_state: :bad_guess, turns_left: game.turns_left - 1}
   end
 
   defp tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
+
+  defp return_with_tally(game) do
+    {game, tally(game)}
+  end
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _letter), do: "_"
 end
