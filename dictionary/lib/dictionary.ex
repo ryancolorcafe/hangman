b2ae3c982d47e6c16f54e7a7441e7792cff6d807
ryancolorcafe <ryan.brown@fastradius.com>
2022-09-23 10:43:51 -0700

    implement Agent, run dictionary as an application

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,11 +1,6 @@
 defmodule Dictionary do
-  alias Dictionary.Impl.WordList
+  alias Dictionary.Runtime.Server
 
-  @opaque t :: WordList.t()
-
-  @spec start() :: t
-  defdelegate start, to: WordList, as: :word_list
-
-  @spec random_word(t) :: String.t()
-  defdelegate random_word(word_list), to: WordList
+  @spec random_word() :: String.t()
+  defdelegate random_word(), to: Server
 end
---
+++ dictionary/lib/runtime/server.ex
@@ -0,0 +1,14 @@
+defmodule Dictionary.Runtime.Server do
+  alias Dictionary.Impl.WordList
+
+  @me __MODULE__
+
+  @type t :: pid()
+  def start_link do
+    Agent.start_link(&WordList.word_list/0, name: @me)
+  end
+
+  def random_word() do
+    Agent.get(@me, &WordList.random_word/1)
+  end
+end
