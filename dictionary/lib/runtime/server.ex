4399ef59af5af4bea05b8b3943b42dee35aa9dd5
ryancolorcafe <ryan.brown@fastradius.com>
2022-10-03 19:48:51 -0700

    add a supervisor for the dictionary

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,14 +1,19 @@
 defmodule Dictionary.Runtime.Server do
+  use Agent
+
   alias Dictionary.Impl.WordList
 
   @me __MODULE__
 
   @type t :: pid()
-  def start_link do
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    if :rand.uniform < 0.33 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
     Agent.get(@me, &WordList.random_word/1)
   end
 end
