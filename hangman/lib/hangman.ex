2825281f58696b0c8769844d77f3c451d00ea228
ryancolorcafe <ryan.brown@fastradius.com>
2022-12-09 12:57:11 -0800

    remove duplicate tally type

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -6,22 +6,15 @@ defmodule Hangman do
   @opaque game :: Server.t()
   @type tally :: Type.tally()
 
-  @type tally :: %{
-          turns_left: integer,
-          game_state: state,
-          letters: list(String.t()),
-          used: list(String.t())
-        }
-
   @spec new_game :: game
   def new_game do
-     {:ok, pid} = Hangman.Runtime.Application.start_game
-     pid
+    {:ok, pid} = Hangman.Runtime.Application.start_game()
+    pid
   end
 
   @spec make_move(game, String.t()) :: tally
-  def make_move(game, guess)do
-     GenServer.call(game, {:make_move, guess})
+  def make_move(game, guess) do
+    GenServer.call(game, {:make_move, guess})
   end
 
   @spec tally(game) :: tally
